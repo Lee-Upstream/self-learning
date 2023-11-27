caae2565956304d54c0b07c56ecbb2fb8dc27b22
LeeUpstream <Leeupstream@Yuzes-MacBook-Pro.local>
2023-11-27 15:19:24 +1030

    router

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// handle router name that finish with "/test"
+Route::get('/test', function(){
+    echo 'Hello World';
+});
+
+Route :: get ('about', function(){
+    return "I am about page!";
+});
+
+// variable, passing parameters
+Route :: get ('/post/{id}/{name}', function($id, $name){
+    return "this is post number " . $id . "with name " . $name;
+});
+
+
+// puting an array in the second parameter that wrapping everything
+Route :: get('admin/posts/example', array('as'=>'admin.home',function(){
+    $url = route('admin.home');
+    return "This url is " . $url ; // php using "." to do string connection
+    // at this time, the admin.home is the same thing as "admin/posits/examples"
+}));
+
+//php artisan route:list
+
